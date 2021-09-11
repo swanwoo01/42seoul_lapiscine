74e1d12b05c27c6935c3a1a7e2a0be1f3b2246c9
swanwoo01 <swanwoo01@gmail.com>
2021-09-11 21:53:49 +0900

    42

--- shell_01/ex07/r_dwssap.sh
+++ shell_01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d':' -f1 | rev | sort -r | awk '{if(NR>='$FT_LINE1' && NR<='$FT_LINE2') {print $0}}'| tr '\n' ',' | sed -e 's/,/, /g' | sed -e 's/,$/\./' | tr -d '\n'
+cat /etc/passwd | grep -v '#' | awk 'NR % 2 == 0' | cut -d':' -f 1 | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | tr '\n' ',' | sed 's/,/, /g' | sed -e 's/, $/./' | tr -d '\n'
