0b9bcafc8e7f97e0d45c0471745039bca6db3782
swanwoo01 <swanwoo01@gmail.com>
2021-09-13 21:57:11 +0900

    42

--- C_01/main.c
+++ C_01/main.c
@@ -1,5 +1,20 @@
+#include "ex00/ft_ft.c"
+#include "ex01/ft_ultimate_ft.c"
+#include "ex02/ft_swap.c"
+#include "ex03/ft_div_mod.c"
+#include "ex04/ft_ultimate_div_mod.c"
 #include "ex05/ft_putstr.c"
+#include "ex06/ft_strlen.c"
+
 #include <stdio.h>
+
+// int main()
+// {
+// 	int nbr;
+// 	ft_ft(&nbr);
+// 	printf("%d", nbr);
+// 	return 0;
+// }
 
 // int main()
 // {
@@ -27,9 +42,37 @@
 // 	return (0);
 // }
 
+// int main()
+// {
+// 	int a = 42;
+// 	int b = 24;
+// 	int div;
+// 	int mod;
+// 	ft_div_mod(a, b, &div, &mod);
+// 	printf("%d %d", div, mod);
+// 	return 0;
+// }
 
-int main()
-{
-	ft_putstr("asdasdasd");
-	return 0;
-}
+// int main()
+// {
+// 	int a = 42;
+// 	int b = 24;
+// 	ft_ultimate_div_mod(&a, &b);
+// 	printf("%d %d", a, b);
+// 	return (0);
+// }
+
+// int main()
+// {
+// 	char *str = "wonwoo king"
+// 	ft_putstr(str);
+// 	return (0);
+// }
+
+// int main()
+// {
+// 	char *str = "wonwoo king";
+// 	int len = ft_strlen(str);
+// 	printf("%d", len);
+// 	return (0);
+// }
